211ddb137eae505c0a8aa0e4840033bfcfc119b2
Mastewal B <se.mastewal.belete@gmail.com>
2021-06-30 10:16:09 +0300

    Min Heap - constructor

---
+++ Priority Queue, Heaps, and Heapsort/MinHeap.py
@@ -0,0 +1,9 @@
+import sys
+
+
+class MinHeap:
+    def __init__(self, maxsize):
+        self.maxsize = maxsize
+        self.size = 0
+        self.Heap = [0] * (self.maxsize + 1)
+        self.Heap[0] = -1 * sys.maxsize
