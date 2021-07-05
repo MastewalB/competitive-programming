0b5bfbc13287af170519ead8aebbf2c002703dc6
Mastewal B <se.mastewal.belete@gmail.com>
2021-07-05 18:03:57 +0300

    Rule to indicate min or max heap

--- Priority Queue, Heaps, and Heapsort/in-place-heap.py
+++ Priority Queue, Heaps, and Heapsort/in-place-heap.py
@@ -33,11 +33,30 @@ class InPlaceHeap:
             array[pos], array[largest] = array[largest], array[pos]
             InPlaceHeap.maxHeapify(array, largest)
 
-    def build_max_heap(array):
+    @staticmethod
+    def minHeapify(array, pos):
+        length = len(array)
+        smallest = pos
+        left = InPlaceHeap.left(pos)
+        right = InPlaceHeap.right(pos)
+
+        if left < length and array[pos] > array[left]:
+            smallest = left
+
+        if right < length and array[smallest] > array[right]:
+            smallest = right
+
+        if smallest != pos:
+            array[pos], array[smallest] = array[smallest], array[pos]
+            InPlaceHeap.minHeapify(array, smallest)
+
+    def build_heap(array, rule):
+
         for i in range(len(array) - 1, -1, -1):
-            InPlaceHeap.maxHeapify(array, i)
+            InPlaceHeap.maxHeapify(
+                array, i) if rule > 0 else InPlaceHeap.minHeapify(array, i)
 
 
 array = [2, 7, 4, 1, 8, 1]
-InPlaceHeap.build_max_heap(array)
+InPlaceHeap.build_heap(array, 0)
 print(array)
