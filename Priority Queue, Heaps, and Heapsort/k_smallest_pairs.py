4005bd5fb7c89cc0d4d167391cf4a5bc93631102
Mastewal B <se.mastewal.belete@gmail.com>
2021-07-08 07:20:06 +0300

    First method to solve k smallest pairs

--- Priority Queue, Heaps, and Heapsort/k_smallest_pairs.py
+++ Priority Queue, Heaps, and Heapsort/k_smallest_pairs.py
@@ -7,3 +7,20 @@ k = 3
 
 def k_smallest_pairs(nums1, nums2, k):
     list_heap = List_Heap(k)
+
+    i = 0
+    j = 0
+    for i in range(len(nums1)):
+        for j in range(len(nums2)):
+            s = nums1[i] + nums2[j]
+            element = [s, [nums1[i], nums2[j]]]
+
+            if list_heap.size >= k:
+                if list_heap.Heap[list_heap.size][0] > element[0]:
+                    list_heap.subsert(element)
+            else:
+                list_heap.insert(element)
+    return list_heap
+
+
+k_smallest_pairs(nums1, nums2, k).Print()
