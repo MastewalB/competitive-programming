68296dcc346db75105549b0221a5a67731366643
Mastewal B <se.mastewal.belete@gmail.com>
2021-07-08 07:24:33 +0300

    using heapsort

--- Priority Queue, Heaps, and Heapsort/k_smallest_pairs.py
+++ Priority Queue, Heaps, and Heapsort/k_smallest_pairs.py
@@ -1,15 +1,14 @@
 from List_Heap import List_Heap
+from InPlaceHeapSort import InPlaceHeapSort
 
 nums1 = [1, 7, 11]
 nums2 = [2, 4, 6]
 k = 3
 
 
-def k_smallest_pairs(nums1, nums2, k):
+def k_smallest_pairs_version_1(nums1, nums2, k):
     list_heap = List_Heap(k)
 
-    i = 0
-    j = 0
     for i in range(len(nums1)):
         for j in range(len(nums2)):
             s = nums1[i] + nums2[j]
@@ -21,6 +20,3 @@ def k_smallest_pairs(nums1, nums2, k):
             else:
                 list_heap.insert(element)
     return list_heap
-
-
-k_smallest_pairs(nums1, nums2, k).Print()
