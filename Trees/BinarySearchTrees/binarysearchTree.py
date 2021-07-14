109a89b207c2cd333ab15fc36b32dbeb07158dce
Mastewal B <se.mastewal.belete@gmail.com>
2021-07-14 07:59:58 +0300

    More complex BST

---
+++ Trees/BinarySearchTrees/binarysearchTree.py
@@ -0,0 +1,72 @@
+import os
+
+
+class BinarySearchTree:
+
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def find(self, value):
+        node = self
+        while node:
+            if value < node.val:
+                node = node.left
+
+            elif value > node.val:
+                node = node.right
+
+            else:
+                return node
+
+    def get_max(self):
+        node = self
+        while node and node.right:
+            node = node.right
+        return node
+
+    def get_min(self):
+        node = self
+        while node and node.left:
+            node = node.left
+        return node
+
+    def insert(self, value, node=None, root=True):
+        if root:
+            node = self
+        if node is None:
+            return BinarySearchTree(value)
+
+        if value < node.value:
+            node.left = self.insert(value, node.left, False)
+
+        elif value > node.value:
+            node.right = self.insert(value, node.right, False)
+
+        else:
+            return node
+
+        return node
+
+    def delete(self, value, node=None, root=True):
+        if root:
+            node = self
+        if node is None:
+            return None
+
+        if value < node.value:
+            node.left = self.delete(value, node.left, False)
+        elif value > node.value:
+            node.right = self.delete(value, node.right, False)
+
+        elif node.left and node.right:
+            successor = self.get_min(node.right)
+            node.val = successor.val
+            node.right = self.delete(node.val, node.right, False)
+
+        else:
+            if node.left is None:
+                node = node.right
+            elif node.right is None:
+                node = node.left
