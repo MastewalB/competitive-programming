e50bef85e968ddcbd31f0fb74413b8eae3d8f6a7
MastewalB <se.mastewal.belete@gmail.com>
2021-07-17 23:52:09 +0300

    BST with node - inserition and inorder traversal

--- Trees/BinarySearchTrees/BinarySearchTree-Node.py
+++ Trees/BinarySearchTrees/BinarySearchTree-Node.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self, value):
         self.index = 0
-        self.value - value
+        self.value = value
         self.parent = None
         self.left = None
         self.right = None
@@ -19,3 +19,94 @@ class Node:
 class BinarySearchTree:
     def __init__(self, root):
         self.root = Node(root)
+
+
+    def find(self, value):
+        node = self.root
+        while node:
+            if value < node.value:
+                node = node.left
+            elif value > node.value:
+                node = node.right
+            
+            else:
+                return node.value
+    
+    def get_max(self):
+        node = self.root
+        while node and node.right:
+            node = node.right
+        
+        return node.value
+
+
+    def insert(self, element):
+        node = self.root
+        parent_node = None
+        new_element = Node(element)
+
+        while True:
+            if element > node.value:
+                parent_node = node
+                node = node.right
+                if node == None:
+                    parent_node.right = new_element
+                    return
+
+            elif element < node.value:
+                parent_node = node
+                node = node.left   
+                if node == None:
+                    parent_node.left = new_element
+                    return
+            
+            else:
+                print("No duplicate elements allowed")
+                return
+    
+
+    def inorder_traversal(self):
+        self.node_list = []
+        self._inorder_traversal(self.root)
+        print(self.node_list)
+    
+    def _inorder_traversal(self, node):
+        if node != None:
+            self._inorder_traversal(node.left)
+            self.node_list.append(node.value)
+            self._inorder_traversal(node.right)
+
+    def print(self, node=None):
+        
+        if node != None:
+            print(node.value, end = " ")
+            self.print(node.left)
+            self.print(node.right)
+
+tree = BinarySearchTree(40)
+tree.insert(20)
+tree.insert(10)
+tree.insert(5)
+tree.insert(30)
+tree.insert(50)
+tree.insert(60)
+tree.insert(67)
+tree.insert(78)
+tree.insert(7)
+tree.insert(45)
+tree.insert(55)
+tree.insert(62)
+##tree = BinarySearchTree(7)
+##tree.insert(6)
+##tree.insert(9)
+##tree.insert(0)
+##tree.insert(2)
+##tree.insert(8)
+##tree.insert(12)
+##tree.insert(1)
+##tree.insert(3)
+##tree.insert(10)
+##tree.insert(11)
+tree.inorder_traversal()
+print(tree.get_max())
+tree.print(tree.root)
