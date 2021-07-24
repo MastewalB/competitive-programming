ef8a5eb2d6aae502d3a885a56b4b11745c09d864
MastewalB <se.mastewal.belete@gmail.com>
2021-07-24 22:00:48 +0300

    added to list successfully

--- Trees/BinarySearchTrees/bottom_view.py
+++ Trees/BinarySearchTrees/bottom_view.py
@@ -1,31 +1,47 @@
 from BinarySearchTree_Node import BinarySearchTree
 
 visibles = []
+
+
 def bottom_view(node, index):
     if node is None:
         return
     
     print(index, "node -", node.value)
+    found = False
+    for i in range(len(visibles)):
+        if visibles[i][0] == index:
+            visibles[i][1] = node.value
+            found = True
+    if not found:        
+        visibles.append([index, node.value])
+    found = False                
+
     if node.left:
         index += 1
         bottom_view(node.left, index)
-    
+
+
         if index >= 0:
             index -= 1
         elif index < 0:                
             index += 1
-
-        
+    
+    
     
     if node.right:
         index -= 1
         bottom_view(node.right, index)
+
+
         if index >= 0:
             index -= 1
         elif index < 0:                
             index += 1
-        
-
+    
+    
+    
+    
     
     
             
@@ -45,3 +61,4 @@ tree.insert(1)
 tree.insert(3)
 
 bottom_view(tree.root, 0)
+print(visibles)
