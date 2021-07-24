23142baa4fcf94c298aa3533ddc02d79389549ef
MastewalB <se.mastewal.belete@gmail.com>
2021-07-24 20:21:15 +0300

    Bug fix

--- Trees/BinarySearchTrees/bottom_view.py
+++ Trees/BinarySearchTrees/bottom_view.py
@@ -4,24 +4,30 @@ visibles = []
 def bottom_view(node, index):
     if node is None:
         return
-    node_index = index
+    
     print(index, "node -", node.value)
     if node.left:
         index += 1
         bottom_view(node.left, index)
-        if index > 0:
-            index -= 2
-        elif index < 0:
-            index += 2
+    
+        if index >= 0:
+            index -= 1
+        elif index < 0:                
+            index += 1
+
         
     
     if node.right:
         index -= 1
         bottom_view(node.right, index)
-        if index > 0:
+        if index >= 0:
             index -= 1
-        elif index < 0:
+        elif index < 0:                
             index += 1
+        
+
+    
+    
             
 
 
@@ -33,9 +39,9 @@ tree.insert(7)
 tree.insert(9)
 tree.insert(12)
 tree.insert(13)
-tree.insert(3)
+tree.insert(2)
 tree.insert(5)
 tree.insert(1)
-tree.insert(2)
+tree.insert(3)
 
 bottom_view(tree.root, 0)
