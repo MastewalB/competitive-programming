d3149ec1bf7ee99526a034e9c48049cebdc89d36
MastewalB <se.mastewal.belete@gmail.com>
2021-07-24 20:00:36 +0300

    Level Indexing

--- Trees/BinarySearchTrees/bottom_view.py
+++ Trees/BinarySearchTrees/bottom_view.py
@@ -1,6 +1,41 @@
 from BinarySearchTree_Node import BinarySearchTree
 
+visibles = []
+def bottom_view(node, index):
+    if node is None:
+        return
+    node_index = index
+    print(index, "node -", node.value)
+    if node.left:
+        index += 1
+        bottom_view(node.left, index)
+        if index > 0:
+            index -= 2
+        elif index < 0:
+            index += 2
+        
+    
+    if node.right:
+        index -= 1
+        bottom_view(node.right, index)
+        if index > 0:
+            index -= 1
+        elif index < 0:
+            index += 1
+            
 
 
-def bottom_view(tree):
-    visibles = []
+tree = BinarySearchTree(8)
+tree.insert(6)
+tree.insert(10)
+tree.insert(4)
+tree.insert(7)
+tree.insert(9)
+tree.insert(12)
+tree.insert(13)
+tree.insert(3)
+tree.insert(5)
+tree.insert(1)
+tree.insert(2)
+
+bottom_view(tree.root, 0)
