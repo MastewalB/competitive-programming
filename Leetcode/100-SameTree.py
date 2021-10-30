6f34dac371b63726d65d63df73e0da47ee185ead
MastewalB <se.mastewal.belete@gmail.com>
2021-10-30 16:58:02 +0300

    Same tree

---
+++ Leetcode/100-SameTree.py
@@ -0,0 +1,23 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def is_same_tree(p, q):
+    if p == None and q == None:
+        return True
+    if p == None or q == None:
+        return False
+
+    if p.val == q.val:
+        return is_same_tree(p.left, q.left) and is_same_tree(p.right, q.right)
+    else:
+        return False
+
+
+p = TreeNode(0, 1, 2)
+q = TreeNode(0, 1, 2)
+
+print(is_same_tree(p, q))
